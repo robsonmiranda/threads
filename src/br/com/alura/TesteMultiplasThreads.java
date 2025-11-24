454f939f7ef1873808b95e265635dd24b1fc52ed
Robson Miranda <rmatosm@gmail.com>
2025-11-24 08:00:14 -0300

    Versão com Java 21 para permitir try with resources com ExecutorService

--- src/br/com/alura/TesteMultiplasThreads.java
+++ src/br/com/alura/TesteMultiplasThreads.java
@@ -4,21 +4,18 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class TesteMultiplasThreads {
 
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(10000); // Cria um pool com 10000 threads
         Instant inicio = Instant.now();
-        for (int i = 1; i <= 10000; i++) {
-            var tarefa = new ExecutaTarefa();
-            executor.execute(tarefa);
+        try (ExecutorService e = executor) {
+            for (int i = 1; i <= 10000; i++) {
+                var tarefa = new ExecutaTarefa();
+                e.execute(tarefa);
+            }
         }
-        executor.shutdown();
-        // Opcional, aguarda as tarefas terminarem
-        // Garante que o tempo calculado só seja medido após todas as tarefas terminarem.
-        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
         Instant fim = Instant.now();
         Duration duracao = Duration.between(inicio, fim);
         System.out.println("Tempo gasto: " + duracao.getSeconds());
